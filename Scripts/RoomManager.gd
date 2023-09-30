8bd53d9e4aadf234966f8012a96bd336da662ef0
Sam Boyer <sam.boyer276yt@gmail.com>
2023-09-30 23:55:02 +0100

    add canvas modulations, sound effects

--- Scripts/RoomManager.gd
+++ Scripts/RoomManager.gd
@@ -16,11 +16,18 @@ var ROOM_PREFAB:PackedScene=null
 @export
 var bm:BeesManager=null
 
+var cm:CanvasModulate=null
+var current_color = Color(1,1,1,1)
+var next_color = Color(1,1,1,1)
+
 var rooms_reached := 0
 
 func load_next_room():
     var new_room:=ROOM_PREFAB.instantiate()
     # @@@ Apply room variations here
+    # Choose a random colour
+    next_color = Color.from_hsv(randf(),randf()*0.1+0.05,1)
+
     new_room.position = Vector2(0, SCREEN_HEIGHT)
     add_child(new_room)
     next_room = new_room
@@ -42,6 +49,7 @@ func finish_transition_to_next_room():
     if current_room:
         current_room.queue_free()
     current_room=next_room
+    current_color = next_color
     next_room=null
     for b in get_tree().get_nodes_in_group("bees"):
         b.position = Vector2(0,SCREEN_HEIGHT/2-16)
@@ -56,6 +64,7 @@ func _process(delta):
         if current_room!=null:
             current_room.position = Vector2(0,SCREEN_HEIGHT*t)
         next_room.position = Vector2(0,SCREEN_HEIGHT*(t-1))
+        cm.color = lerp(current_color,next_color,t)
         if room_transition_t>=1:
             finish_transition_to_next_room()
 
@@ -64,5 +73,6 @@ func make_and_transition_to_room():
     start_transition_to_next_room()
 
 func _ready():
+    cm = get_tree().get_first_node_in_group("CanvasModulate")
     load_next_room()
     finish_transition_to_next_room() # skip transition

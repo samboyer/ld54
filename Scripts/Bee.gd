f5609c3876d082a7de16a7cdd4fb4836c235da75
Josh Smailes <joshua.smailes@cs.ox.ac.uk>
2023-09-30 11:24:25 +0100

    Convert to spaces

--- Scripts/Bee.gd
+++ Scripts/Bee.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 var vel := Vector2(0,0)
@@ -13,14 +13,14 @@ var accel := 10000
 var jitter: int = 100
 
 func rand_range(min_val: int, max_val: int) -> int:
-	return randi() % (max_val - min_val) + min_val
+    return randi() % (max_val - min_val) + min_val
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var mouse_pos: Vector2 = self.get_parent().get_local_mouse_position()
-	var dir = (mouse_pos - self.position).normalized() * accel
-	vel += dir * delta
-	vel += Vector2(rand_range(-jitter, jitter), rand_range(-jitter, jitter))
-	vel = vel.limit_length(maxspeed)
-	velocity = vel
-	move_and_slide()
+    var mouse_pos: Vector2 = self.get_parent().get_local_mouse_position()
+    var dir = (mouse_pos - self.position).normalized() * accel
+    vel += dir * delta
+    vel += Vector2(rand_range(-jitter, jitter), rand_range(-jitter, jitter))
+    vel = vel.limit_length(maxspeed)
+    velocity = vel
+    move_and_slide()
--- Scripts/BeesManager.gd
+++ Scripts/BeesManager.gd
@@ -11,21 +11,21 @@ var bees:Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(NUM_BEES):
-		var x = beeObj.instantiate()
-		add_child(x)
-		x.position = Vector2(randi() % 1000 - 500, randi() % 1000 - 500)
-		bees.append(x)
+    for i in range(NUM_BEES):
+        var x = beeObj.instantiate()
+        add_child(x)
+        x.position = Vector2(randi() % 1000 - 500, randi() % 1000 - 500)
+        bees.append(x)
 
 var centre_of_mass:Vector2 = Vector2.ZERO
 
 # # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	centre_of_mass = Vector2.ZERO
-	for b in bees:
-		centre_of_mass += b.position
-	centre_of_mass /= NUM_BEES
-	queue_redraw()
+    centre_of_mass = Vector2.ZERO
+    for b in bees:
+        centre_of_mass += b.position
+    centre_of_mass /= NUM_BEES
+    queue_redraw()
 
 func _draw():
-	draw_circle(centre_of_mass, 10, Color(1, 0, 0))
+    draw_circle(centre_of_mass, 10, Color(1, 0, 0))
